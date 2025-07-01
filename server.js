f4fe0f64f569f6f28e5eff71911067f41f737b4b
Domenico Lupo <domenicolupo.ce@gmail.com>
2025-07-01 16:00:45 +0200

    change port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3001;
+const PORT = 3051;
 const DATA_DIR = path.join(__dirname, 'data');
 
 app.use(cors());
